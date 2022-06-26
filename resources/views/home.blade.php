21e743e7c88e026c69484721314067e374aab7f0
nt1121 <springtree3231@gmail.com>
2022-06-26 17:04:54 +0900

    home画面の追加

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="天気 現在の天気">
+    <meta name="description" content="日本の都市の現在の天気を確認できるWebアプリです。">
+    <title>Current Weather</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="l-wrap">
+        <div class="l-header">
+            <div class="p-header">
+                <div class="p-header__inner">
+                    <div class="p-header__logo">
+                        Current Weather
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="l-content">
+            <div class="l-content__inner">
+            </div>
+        </div>
+        <div class="l-footer">
+            <div class="p-footer">
+                <div class="p-footer__inner">
+                    <span class="p-footer__copyright">&copy;</span> 2022 nt1121
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
